fa9c92e5dba8b64f37dc1beff0c0d7c1d17bd141
Arya <aryawirasandi7153@gmail.com>
2019-08-29 23:35:51 +0700

    first commit

---
+++ pages/Jokes/_id/index.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <nuxt-link  to="/jokes">
+            Back To Jokes
+        </nuxt-link>
+        <h2>{{joke}}</h2>
+        <small>Joke ID : {{$route.params.id}}</small>
+    </div>
+</template>
+<script>
+import axios from 'axios';
+export default {
+    data:() => ({
+        joke : []
+    }),
+     async created(){
+         const config = {
+             headers:{
+                 'Accept' : 'application/json'
+             }
+         }
+        try{
+            const res = await axios.get(`https://icanhazdadjoke.com/j/${this.$route.params.id}`, config)
+            this.joke = res.data.joke;
+            console.log(this.joke)
+        }catch(err){
+            console.log(err);
+        }
+         
+     },
+     head(){
+        return {
+            title : this.joke,
+            meta : [
+                {
+                    hid : 'description',
+                    name : 'description',
+                    content : 'Best Place For Corny Dad Jokes'
+                }
+            ]
+        }
+    }
+}
+</script>
+<style>
+
+</style>
---
+++ pages/about.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>About Bad Jokes</h1>
+        <p>This is a app that display corny dad jokes</p>
+    </div>    
+</template>
+<script>
+export default {
+    head(){
+        return {
+            title : 'About The App',
+            meta : [
+                {
+                    hid : 'description',
+                    name : 'description',
+                    content : 'Best Place For Corny Dad Jokes'
+                }
+            ]
+        }
+    }
+}
+</script>
+<style scoped>
+
+</style>
